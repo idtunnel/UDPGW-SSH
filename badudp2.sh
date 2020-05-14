9403d17fa2157f8ccb0bd14f67da1b6c0655021a
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-05-14 15:00:53 +0700

    Create badudp2.sh

---
+++ badudp2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Created by Mei 2020
+# Modified by IDTunnel
+
+# install badvpn
+cd
+wget -O /bin/badvpn-udpgw "https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-udpgw"
+#port BadVPN 7300
+sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
+
+#permission
+chmod +x /usr/bin/badvpn-udpgw
+chomod +x /etc/rc.d/rc.local
+
+#badvpn 7300
+ screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
