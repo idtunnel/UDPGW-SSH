9919040e976a6bfc94eb1156912fd25e9b99b19a
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-05-24 03:57:58 +0700

    Update badudp2.sh

--- badudp2.sh
+++ badudp2.sh
@@ -9,8 +9,6 @@ wget -O /usr/bin/badvpn-udpgw "https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-
 
 #port BadVPN 7300
 sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
-sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7200' /etc/rc.local
-sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7100' /etc/rc.local
 
 #permission
 chmod +x /usr/bin/badvpn-udpgw
@@ -18,4 +16,4 @@ chmod +x /bin/badvpn-udpgw
 chmod +x /etc/rc.d/rc.local
 
 #coba jalankan badvpn 7300
- screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
