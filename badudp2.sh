50092768848adea9febc98ab0c0e19e567026ba8
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-05-24 03:59:32 +0700

    Update badudp2.sh

--- badudp2.sh
+++ badudp2.sh
@@ -2,6 +2,8 @@
 # Created by Mei 2020
 # Modified by IDTunnel
 
+#debian Bad UDP
+
 # install badvpn
 cd
 wget -O /bin/badvpn-udpgw "https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-udpgw"
@@ -13,7 +15,7 @@ sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300' /
 #permission
 chmod +x /usr/bin/badvpn-udpgw
 chmod +x /bin/badvpn-udpgw
-chmod +x /etc/rc.d/rc.local
+chmod +x /etc/rc.local
 
 #coba jalankan badvpn 7300
 screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
