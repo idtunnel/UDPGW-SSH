9b6419d32e29cb5b3b6d9fb8d9d8ba8e6a5385c6
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-06-10 13:10:25 +0700

    Create cenbadvpn.sh

---
+++ cenbadvpn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Created by Mei 2020
+# Modified by IDTunnel
+
+#debian Bad UDP
+
+# install badvpn
+cd
+wget -O /bin/badvpn-udpgw "https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-udpgw"
+
+#port BadVPN 7300
+sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+' /etc/rc.local
+sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+' /etc/rc.d/rc.local
+
+#permission
+chmod +x /usr/bin/badvpn-udpgw
+chmod +x /bin/badvpn-udpgw
+chmod +x /etc/rc.local
+/etc/rc.d/rc.local
+
+#coba jalankan badvpn 7300
+screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+
+#hapus
+rm -rf cenbadvpn.sh
